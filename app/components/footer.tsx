99c67c0d31c05d37891d6e6235f6fdf93c96d0e7
Ugonna <ugonnagodson512@gmail.com>
2023-08-01 13:38:49 +0100

    first commit

---
+++ app/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="bg-white border-4">
+			<div className="mx-auto py-18">
+				<p className="text-center text-xs text-black">
+					&copy; 2023 E-Store, Inc. All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
